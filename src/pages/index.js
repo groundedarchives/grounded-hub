e6cdfd14d47a2cd4d9d25189ed81437b1ecca1d9
Akshat Sharma <akshatksharma@wustl.edu>
2021-03-15 22:08:24 -0500

    Reorganized styles and main page structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,19 @@ import "../styles/home.css"
 export default function Home() {
   return (
     <>
-      <nav></nav>
-      <section className="landing"></section>
-      <section className="about"></section>
-      <section className="explore"></section>
-      <footer>
-        <h1 className="title">Footer</h1>
-      </footer>
+      <nav>
+        
+      </nav>
+      <section className="landing">
+        <h1 className="title-L">Home</h1>
+      </section>
+      <section className="about">
+        <h1 className="title-L">About</h1>
+      </section>
+      <section className="explore">
+        <h1 className="title-L">Explore</h1>
+      </section>
+      <footer></footer>
     </>
   )
 }
