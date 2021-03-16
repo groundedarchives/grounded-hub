45ba8eb84464a469ee0d73451e4ef80a296ec427
Akshat Sharma <akshatksharma@wustl.edu>
2021-03-15 21:50:45 -0500

    setup initial structure of main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
+import "../styles/global.css"
+import "../styles/home.css"
 
 export default function Home() {
   return (
     <>
-      <h1>Grounded site</h1>
-      <div>Hello world</div>
+      <nav></nav>
+      <section className="landing"></section>
+      <section className="about"></section>
+      <section className="explore"></section>
+      <footer>
+        <h1 className="title">Footer</h1>
+      </footer>
     </>
   )
 }
