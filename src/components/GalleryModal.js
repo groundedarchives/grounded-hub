805a891ffe95de03e5104f6988c03561f12c537c
Akshat Sharma <akshatksharma@wustl.edu>
2021-04-24 13:31:15 -0500

    Created hamburger menu on mobile + tablet

--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -1,78 +1,79 @@
-import React, { Component, useState , useEffect} from "react";
-import { SlideDown } from 'react-slidedown'
-import 'react-slidedown/lib/slidedown.css'
+import React, { Component, useState, useEffect } from "react"
+import { SlideDown } from "react-slidedown"
+import "react-slidedown/lib/slidedown.css"
 import Card from "react-bootstrap/Card"
 import "../styles/gallery.css"
 
 class Collapsible extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            width: 0,
-            height:0,
-            pictureWidth: this.props.width,
-            pictureHeight: this.props.height
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      width: 0,
+      height: 0,
+      pictureWidth: this.props.width,
+      pictureHeight: this.props.height,
+    }
+    this.togglePanel = this.togglePanel.bind(this)
+  }
 
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener("resize", this.updateWindowDimensions.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions.bind(this))
+  }
+
+  updateWindowDimensions() {
+    this.setState(
+      {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+      () => {
+        if (this.state.width < 500) {
+          let tempWidth = this.state.pictureWidth / 3
+          let tempHeight = this.state.pictureHeight / 3
+          this.setState({
+            pictureHeight: tempHeight,
+            pictureWidth: tempWidth,
+          })
         }
-        this.togglePanel = this.togglePanel.bind(this);
+      }
+    )
+  }
+  togglePanel(e) {
+    this.setState({ open: !this.state.open })
+  }
+
+  render() {
+    const mystyle = {
+      maxWidth: this.state.pictureWidth,
+      maxHeight: this.state.pictureHeight,
     }
-
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener("resize", this.updateWindowDimensions.bind(this));
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWindowDimensions.bind(this));
-    }
-
-    updateWindowDimensions() {
-        this.setState({ 
-            width: window.innerWidth, 
-            height: window.innerHeight,
-            }, () => {
-                if (this.state.width < 500){
-                    let tempWidth = this.state.pictureWidth/3;
-                    let tempHeight = this.state.pictureHeight/3;
-                    this.setState({
-                        pictureHeight: tempHeight,
-                        pictureWidth: tempWidth
-                    })
-
-                }
-        
-
-            })
-    } 
-    togglePanel(e) {
-        this.setState({ open: !this.state.open })
-    }
-
-
-    render() {
-
-        const mystyle = {
-            maxWidth: this.state.pictureWidth,
-            maxHeight: this.state.pictureHeight,
-
-        };
-        return (<div>
-            <div style={{width: this.state.pictureWidth}} onClick={(e) => this.togglePanel(e)} className='header'>
-                <img alt="image" src={this.props.src} style={mystyle}></img>
-                </div>
-            {this.state.open ? (
-                <SlideDown className='content'>
-                        <div style={{width: this.state.pictureWidth}}>
-                        <div>
-                            <h1>:)</h1>
-                            <p> My content</p>
-                        </div>
-                    </div>
-                </SlideDown>
-            ) : null}
-        </div>);
-    }
+    return (
+      <div>
+        <div
+          style={{ width: this.state.pictureWidth }}
+          onClick={e => this.togglePanel(e)}
+          className="header"
+        >
+          <img alt="image" src={this.props.src} style={mystyle}></img>
+        </div>
+        {this.state.open ? (
+          <SlideDown className="content">
+            <div style={{ width: this.state.pictureWidth }}>
+              <div>
+                <h1>:)</h1>
+                <p> My content</p>
+              </div>
+            </div>
+          </SlideDown>
+        ) : null}
+      </div>
+    )
+  }
 }
-export default Collapsible;
+export default Collapsible
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,63 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import SlideOut from "@ijsto/react-slideout"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+
 import "../styles/global.css"
 import "../styles/nav.css"
 
 export default function Nav() {
+  const [slideOutIsOpen, setSlideOutIsOpen] = useState(false)
+  const isDesktop = useMediaQuery("(min-width:768px)")
+
+  const openSlideOut = () => {
+    setSlideOutIsOpen(true)
+  }
+
+  const closeSlideOut = () => {
+    setSlideOutIsOpen(false)
+  }
+
+  const mobileOrDesktopNav = () => {
+    if (isDesktop === null) return null
+    if (isDesktop) {
+      return (
+        <>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/visualization">Visualization</Link>
+          <Link to="/about">About</Link>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <svg
+            onClick={openSlideOut}
+            viewBox="0 0 100 50"
+            width="40"
+            height="40"
+          >
+            <rect width="80" height="8" rx="8"></rect>
+            <rect y="25" width="80" height="8" rx="8"></rect>
+            <rect y="50" width="80" height="8" rx="8"></rect>
+          </svg>
+          <SlideOut
+            isOpen={slideOutIsOpen}
+            onClose={closeSlideOut}
+            offsetTop={0}
+            slideFrom="right"
+          >
+            <h2>My Slide Out</h2>
+          </SlideOut>
+        </>
+      )
+    }
+  }
+
   return (
     <nav>
       <Link to="/">Grounded</Link>
-      <div className="subpages">
-        <Link to="/gallery">Gallery</Link>
-        <Link to="/visualization">Visualization</Link>
-        <Link to="/about">About</Link>
-      </div>
+      <div className="subpages">{mobileOrDesktopNav()}</div>
     </nav>
   )
 }
