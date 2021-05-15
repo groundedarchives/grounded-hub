bda843adb36b3f83d6c3cbaf540b8a4d5a8b79de
Isabel Silverman <isabelsilverman@wustl.edu>
2021-05-15 17:12:21 -0400

    audio and transcriptions

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,27 +1,25 @@
 import React from "react"
 import "../styles/global.css"
 import "../styles/index.css"
-import ReactAudioPlayer from 'react-audio-player';
+import AudioPlayer from 'react-h5-audio-player';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 export default class GalleryItem extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            name: props.name,
-            transcription: props.transcription
-        }
-    }
-
     render() {
         return (
             <div>
-                <img src={this.state.name}></img>
-                <audio className="audio-player"></audio>
+                <img src={this.props.name}></img>
+                <AudioPlayer
+                className="audio-player"
+                src={this.props.audio}
+                customAdditionalControls={[]}
+                customVolumeControls={[]}
+                defaultCurrentTime="" 
+                defaultDuration=""
+                ></AudioPlayer>
                 <Popup trigger={<button> See Full Transcription</button>} modal>
-                    <div>{this.state.transcription}</div>
+                    <div>{this.props.transcription}</div>
                 </Popup>
             </div>
         )
